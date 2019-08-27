6dcac14f1f597bb3c2b7b9ec8a4c6987e1fd0858
Colin Campbell <colinc86@gmail.com>
2019-08-26 20:53:22 -0500

    Parallelize window functions.

--- window.go
+++ window.go
@@ -3,16 +3,21 @@ package gdsp
 import (
 	"math"
 	"math/cmplx"
+	"runtime"
+
+	"github.com/colinc86/parallel"
 )
 
 // Hann performs Hann windowing on the input vector.
 func Hann(input VectorComplex) VectorComplex {
 	vh := input.Copy()
 	theta := 2.0 * math.Pi / float64(len(vh)-1)
-	for i := 0; i < len(vh); i++ {
+
+	p := parallel.NewFixedProcess(runtime.NumCPU())
+	p.Execute(len(vh), func(i int) {
 		x := complex(theta*float64(i), 0)
 		vh[i] *= (1.0 - cmplx.Cos(x)) / 2.0
-	}
+	})
 	return vh
 }
 
@@ -20,10 +25,12 @@ func Hann(input VectorComplex) VectorComplex {
 func InverseHann(input VectorComplex) VectorComplex {
 	vih := input.Copy()
 	theta := 2.0 * math.Pi / float64(len(vih)-1)
-	for i := 0; i < len(vih); i++ {
+
+	p := parallel.NewFixedProcess(runtime.NumCPU())
+	p.Execute(len(vih), func(i int) {
 		x := complex(theta*float64(i), 0)
 		vih[i] /= (1.0 - cmplx.Cos(x)) / 2.0
-	}
+	})
 	return vih
 }
 
@@ -33,10 +40,11 @@ func Hamming(input VectorComplex) VectorComplex {
 	theta := 2.0 * math.Pi / float64(len(vh)-1)
 	a := complex(25.0/46.0, 0.0)
 
-	for i := 0; i < len(vh); i++ {
+	p := parallel.NewFixedProcess(runtime.NumCPU())
+	p.Execute(len(vh), func(i int) {
 		x := complex(theta*float64(i), 0)
 		vh[i] *= a - (1.0-a)*cmplx.Cos(x)
-	}
+	})
 	return vh
 }
 
@@ -46,10 +54,11 @@ func InverseHamming(input VectorComplex) VectorComplex {
 	theta := 2.0 * math.Pi / float64(len(vih)-1)
 	a := complex(25.0/46.0, 0.0)
 
-	for i := 0; i < len(vih); i++ {
+	p := parallel.NewFixedProcess(runtime.NumCPU())
+	p.Execute(len(vih), func(i int) {
 		x := complex(theta*float64(i), 0)
 		vih[i] /= a - (1.0-a)*cmplx.Cos(x)
-	}
+	})
 	return vih
 }
 
@@ -62,10 +71,11 @@ func Nuttal(input VectorComplex) VectorComplex {
 	a2 := complex(0.144232, 0.0)
 	a3 := complex(0.012604, 0.0)
 
-	for i := 0; i < len(vh); i++ {
+	p := parallel.NewFixedProcess(runtime.NumCPU())
+	p.Execute(len(vh), func(i int) {
 		x := complex(theta*float64(i), 0)
 		vh[i] *= a0 - a1*cmplx.Cos(x) + a2*cmplx.Cos(2.0*x) + a3*cmplx.Cos(3.0*x)
-	}
+	})
 	return vh
 }
 
@@ -78,9 +88,10 @@ func InverseNuttal(input VectorComplex) VectorComplex {
 	a2 := complex(0.144232, 0.0)
 	a3 := complex(0.012604, 0.0)
 
-	for i := 0; i < len(vih); i++ {
+	p := parallel.NewFixedProcess(runtime.NumCPU())
+	p.Execute(len(vih), func(i int) {
 		x := complex(theta*float64(i), 0)
 		vih[i] /= a0 - a1*cmplx.Cos(x) + a2*cmplx.Cos(2.0*x) + a3*cmplx.Cos(3.0*x)
-	}
+	})
 	return vih
 }
